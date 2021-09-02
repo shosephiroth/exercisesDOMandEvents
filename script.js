b45a16efe4a794555e9f1d0829bc7bcaec34b45d
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-09-02 01:04:20 +0000

    finished the first part of the exercise

--- script.js
+++ script.js
@@ -4,6 +4,20 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
+
+    // add button event listener
+    // anonymous function
+    // take button variable/element as parameter
+    // update paragraph element/varible .innerHTML
+  
+    button.addEventListener("click", (button) => {
+        paragraph.innerHTML = "Houston, we have liftoff!";
+    });
+
+
+
+   
+
     
 }
 
